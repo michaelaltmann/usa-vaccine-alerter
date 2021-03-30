92cfee14f12800f52a0dc167ca2e69373395abde
Patrick Demers <patrickdemers6@gmail.com>
2021-03-30 14:18:35 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,47 @@
+# USA Vaccine Availability Aleter Script
+
+This script uses the API provided by [covid-vaccine-spotter](https://github.com/GUI/covid-vaccine-spotter) to send a text message when a vaccine has been located.
+
+## Setup
+
+### Using Twilio
+
+1. Install requirements by running `pip install -r requirements.txt`
+2. Setup a Twilio account. Free trial link: [(refferal)](www.twilio.com/referral/WTOydE) [(no refferal)](https://www.twilio.com/try-twilio)
+   - Get a phone number
+   - Note your phone number, SID, and Auth Token
+3. Open `script.py`
+4. In the config dictionary, customize the values to setup your search parameters.
+
+```python
+config = {
+          'state': 'IA',  # two letter identifier (AL, AK, etc)
+          'cities': ['Ames'],  # name of cities to monitor
+          'zip_codes': ['50012'],  # zip codes to monitor
+          'to': '+15555555555', # phone number to send to
+          'from': '+15555555555' # phone number to send from,
+        }
+```
+
+4. Save the updated file.
+5. Call the program using `python3 script.py SID AUTH_TOKEN` where SID and AUTH_TOKEN are replaced with your proper tokens from twilio.
+
+### Not Using Twilio (output printed to CLI)
+
+1. Install requirements by running `pip install -r requirements.txt`
+2. Open `script.py`
+3. In the config dictionary, customize the values to setup your search parameters. Note that `to` and `from` are not needed.
+
+```python
+config = { 'state': 'IA',  # two letter identifier (AL, AK, etc)
+          'cities': ['Ames'],  # name of cities to monitor
+          'zip_codes': ['50012'],  # zip codes to monitor
+        }
+```
+
+3. Save the updated file.
+4. Call the program using `python3 script.py`
+
+## MLH
+
+Created for Major League Hacks: Share challenge, day 3.
---
+++ script.py
@@ -0,0 +1,80 @@
+from twilio.rest import Client
+import requests
+import threading
+import datetime
+import sys
+
+# Populate with your own configuration data
+# zip codes and cities must have one match (if you want your entire city, you can leave zip_codes list blank)
+config = {'state': 'IA',  # two letter identifier (AL, AK, etc)
+          'cities': ['Ames'],  # name of cities to monitor
+          'zip_codes': ['50012'],  # zip codes to monitor
+          'to': '+17122235883',  # phone number to send to +15555555555
+          # phone number to send from (listed in twilio) +15555555555
+          'from': '+17603891374'
+          }
+
+
+# Twilio initialization
+use_twilio = len(sys.argv[1:]) == 2
+if use_twilio:
+    account_sid = sys.argv[1]  # twilio account SID
+    auth_token = sys.argv[2]  # twilio account auth token
+    client = Client(account_sid, auth_token)
+
+
+def check_appointments():
+    data = requests.get(
+        'https://www.vaccinespotter.org/api/v0/states/{state}.json'.format(state=config['state'])).json()
+    appointment = find_appointment(data)
+    if (appointment != None):
+        message = compose_message(appointment)
+        if use_twilio:
+            send_message(message,
+                         config['to'], config['from'])
+        print(f"{time()} - Appointment found!\n\n{message}")
+        exit(0)
+    print(f"{time()} - No appointments found. ")
+
+
+def compose_message(appointment):
+    return f"COVID-19 vaccine located! {appointment['properties']['provider_brand_name']} in {appointment['properties']['city']}, {appointment['properties']['state']}. Book ASAP:\n\n{appointment['properties']['url']}"
+
+
+def find_appointment(data):
+    for feature in data['features']:
+        if ((feature['properties']['postal_code'] in config['zip_codes'] or
+                feature['properties']['city'].lower() in list_to_lower(config['cities'])) and feature['properties']['appointments_available']):
+            return feature
+    return None
+
+
+def send_message(message, to, from_):
+    message = client.messages.create(
+        body=message, to=to, from_=from_)
+    print(f"Text message sent to {to}!")
+    return message.sid
+
+
+def time():
+    return str(datetime.datetime.now())
+
+
+def list_to_lower(uppercase_list: list):
+    return [i.lower() for i in uppercase_list]
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+# initial check
+check_appointments()
+
+# check every 60 seconds
+set_interval(check_appointments, 60)
